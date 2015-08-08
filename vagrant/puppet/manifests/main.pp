542b4dd466d5e930545f156ff81ccbd327ed4513
Karl Purkhardt <karl.purkhardt@gmail.com>
2015-08-08 22:42:25 +0100

    Removed mongodb and redis

--- vagrant/puppet/manifests/main.pp
+++ vagrant/puppet/manifests/main.pp
@@ -53,25 +53,6 @@ class nodejs {
   }
 }
 
-class mongodb {
-  class {'::mongodb::globals':
-    manage_package_repo => true,
-    bind_ip             => ["127.0.0.1"],
-  }->
-  class {'::mongodb::server':
-    port    => 27017,
-    verbose => true,
-    ensure  => "present"
-  }->
-  class {'::mongodb::client': }
-}
-
-class redis-cl {
-  class { 'redis': }
-}
-
 include apt_update
 include othertools
 include nodejs
-include mongodb
-include redis-cl
